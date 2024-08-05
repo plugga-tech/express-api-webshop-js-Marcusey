2c78e339d668be4d0218da2b88ae9c9542a7f33c
Marcusey <146931671+Marcusey@users.noreply.github.com>
2024-08-05 02:36:23 +0200

    added important installs

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { MongoClient } = require('mongodb');
+const mongoClient = require('mongodb').MongoClient;
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoClient.connect("mongodb://127.0.0.1:27017")
+.then(client => {
+    console.log("Nu är du uppkopplad mot databasen");
+
+    const db = client.db("marcus-yttermyr");
+    app.locals.db = db;
+});
+
 
 app.use(logger('dev'));
 app.use(express.json());
