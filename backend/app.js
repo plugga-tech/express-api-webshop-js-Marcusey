81db0549c615c8d4f3a134a1965ef779df2dd84d
Marcusey <146931671+Marcusey@users.noreply.github.com>
2024-08-20 09:17:24 +0200

    Added users correctly

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const { MongoClient } = require('mongodb');
-const mongoClient = require('mongodb').MongoClient;
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+const mongoClient = require('mongodb').MongoClient;
 mongoClient.connect("mongodb://127.0.0.1:27017")
 .then(client => {
     console.log("Nu är du uppkopplad mot databasen");
@@ -19,7 +18,6 @@ mongoClient.connect("mongodb://127.0.0.1:27017")
     app.locals.db = db;
 });
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,6 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,75 @@
+
 var express = require('express');
+const { ObjectId } = require('mongodb');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET all users */
+router.get('/', async (req, res, next) => {
+  try {
+    const db = req.app.locals.db;
+    const usersCollection = db.collection('users');
+    const userList = await usersCollection.find({}, { projection: { password: 0 } }).toArray();
+    res.json(userList);
+  } catch (err) {
+    console.error('Error fetching users:', err);
+    res.status(500).json({ error: 'Unable to retrieve users' });
+  }
 });
+
+/* GET user by ID */
+router.post('/', async (req, res) => {
+  const { id: userId } = req.body;
+
+  try {
+    const user = await req.app.locals.db.collection('users').findOne({ _id: new ObjectId(userId) });
+
+    if (!user) {
+      return res.status(404).json({ error: 'There is no such user' });
+    }
+
+    return res.json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+/* Create user */
+router.post('/add', async (req, res) => {
+  const user = req.body;
+
+  try {
+    const result = await req.app.locals.db.collection('users').insertOne(user);
+    return res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Could not create new user' });
+  }
+});
+
+/* Login user */
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await req.app.locals.db.collection('users').findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: 'Could not find that user! Please try another email.' });
+    }
+    if (user.password !== password) {
+      return res.status(401).json({ error: 'Wrong password! Please try again!' });
+    }
+    return res.status(200).json({ success: true, message: 'Login success! Welcome back!' });
+  } catch (error) {
+    console.error('Login error:', error);
+    return handleServerError(res);
+  }
+});
+
+function handleServerError(res) {
+  return res.status(500).json({ error: 'Something happend at our server. Try again later.' });
+}
+
 
 module.exports = router;
